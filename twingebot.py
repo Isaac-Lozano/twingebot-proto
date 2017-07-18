3241c50ce5d3cac990f89a9afac3a3c75bcf4071
Isaac Lozano <109lozanoi@gmail.com>
2017-07-17 22:39:06 -0700

    initial commit

---
+++ twingebot.py
@@ -0,0 +1,117 @@
+import twitchirc
+import asyncio
+import re
+import traceback
+
+class TwingeBot(twitchirc.TwitchIrc):
+    def __init__(self, host, port, nick, password, **params):
+        super(TwingeBot, self).__init__(host, port, nick, password, **params)
+        self.channels = list()
+        self.commands = dict()
+        self.regexes = list()
+
+    async def on_welcome(self, irc_dict):
+        print("connected to twitch IRC")
+        # Join all channels
+        for channel in self.channels:
+            self.send_join(channel)
+
+    async def on_privmsg(self, irc_dict):
+        # Check number of args to make sure we have both
+        # channel and message
+        arguments = irc_dict['arguments']
+        if len(arguments) < 2:
+            return
+        channel = arguments[0]
+        message = arguments[1]
+
+        # Is this potentially a command?
+        if message.startswith('!'):
+            # Returns true if we should stop processing
+            if self.on_command(channel, message, irc_dict):
+                return
+
+        # Does this match a regex?
+        for regex, func in self.regexes:
+            if regex.search(message):
+                try:
+                    # Get the response to the trigger
+                    response = func(irc_dict)
+                    self.send_privmsg(channel, response)
+                except Exception as e:
+                    # Don't die if the command had an exception.
+                    # It should actually mostly be fine.
+                    # Instead, print out what went wrong.
+                    traceback.print_exc()
+                return
+
+    def on_command(self, channel, message, irc_dict):
+        # TODO: Possibly extract arguments for the command?
+        command = message[1:].split(' ', 1)[0]
+        try:
+            func = self.commands[command]
+        except KeyError:
+            return False 
+
+        try:
+            # Get the response to the command
+            response = func(irc_dict)
+            self.send_privmsg(channel, response)
+        except Exception as e:
+            # Don't die if the command had an exception.
+            # It should actually mostly be fine.
+            # Instead, print out what went wrong.
+            traceback.print_exc()
+        return True
+
+    async def timer(self, seconds, func):
+        while True:
+            await asyncio.sleep(seconds)
+            try:
+                # Get the response to the trigger
+                response = func()
+                # Send response to all channels
+                for channel in self.channels:
+                    self.send_privmsg(channel, response)
+            except Exception as e:
+                # Don't die if the command had an exception.
+                # It should actually mostly be fine.
+                # Instead, print out what went wrong.
+                traceback.print_exc()
+
+    def set_channels(self, channels):
+        self.channels = channels
+
+    def connect_command(self, command, func):
+        if isinstance(func, str):
+            func = string_func_helper(func)
+        self.commands[command] = func
+
+    def connect_regex(self, regex_str, func):
+        if isinstance(func, str):
+            func = string_func_helper(func)
+        regex = re.compile(regex_str)
+        self.regexes.append((regex, func))
+
+    def connect_timer(self, seconds, func):
+        if isinstance(func, str):
+            func = string_func_helper(func)
+        asyncio.ensure_future(self.timer(seconds, func))
+
+def string_func_helper(string):
+    def return_string(*params):
+        return string
+    return return_string
+
+if __name__ == '__main__':
+    # Create a new twingebot
+    twingebot = TwingeBot('irc.twitch.tv', 6667, 'twingebot', 'OAUTH HERE')
+    # Takes in a list (aka array). channel names should be lowercase and start with a '#'
+    twingebot.set_channels(['#darktwinge'])
+    # Second arguments can also be a function if wanted
+    twingebot.connect_command('sibg', 'There is no song currently playing.')
+    twingebot.connect_regex('so much for heroes', 'Something something so much for heroes.')
+    # Timer is in seconds (Can be a decimal value)
+    twingebot.connect_timer(10, "Follow Like Subscribe.")
+    # Run the bot
+    twingebot.run()
---
+++ twitchirc.py
@@ -0,0 +1,116 @@
+import asyncio
+
+class TwitchIrc(object):
+    def __init__(self, host, port, nick, password, **params):
+        self.host = host
+        self.port = port
+        self.nick = nick
+        self.password = password
+
+        if 'loop' in params:
+            self.loop = params.loop
+        else:
+            self.loop = asyncio.get_event_loop()
+
+        self.reader = None
+        self.writer = None
+        self.buf = bytearray()
+
+    def run(self):
+        self.loop.run_until_complete(self.start_bot())
+
+    async def start_bot(self):
+        await self.connect()
+        await self.process()
+
+    async def connect(self):
+        self.reader, self.writer = await asyncio.open_connection(self.host, self.port)
+        self.send_raw('CAP REQ :twitch.tv/tags twitch.tv/commands twitch.tv/membership')
+        self.send_pass(self.password)
+        self.send_nick(self.nick)
+
+    async def process(self):
+        while True:
+            self.buf += await self.reader.read(4096)
+            while b'\r\n' in self.buf:
+                irc_line = self.buf.partition(b'\r\n')
+                await self.process_line(irc_line[0])
+                self.buf = irc_line[2]
+
+    async def process_line(self, irc_line):
+        irc_str = irc_line.decode('utf-8')
+        irc_dict = dict()
+
+        if irc_str.startswith('@'):
+            temp = irc_str.partition(' ')
+            twitch_str = temp[0][1:]
+
+            twitch_map = dict()
+            for key_value in twitch_str.split(';'):
+                key, _, value = key_value.partition('=')
+                twitch_map[key] = value
+            irc_dict['twitch'] = twitch_map
+
+            irc_str = temp[2]
+
+        if irc_str.startswith(':'):
+            temp = irc_str.partition(' ')
+            irc_dict['from'] = temp[0][1:]
+            irc_str = temp[2]
+
+        temp = irc_str.partition(' ')
+        irc_dict['command'] = temp[0]
+        irc_str = temp[2]
+
+        irc_dict['arguments'] = list()
+        while irc_str:
+            temp = irc_str.partition(' ')
+            if temp[0].startswith(':'):
+                irc_dict['arguments'].append(irc_str[1:])
+                break
+            else:
+                irc_dict['arguments'].append(temp[0])
+            irc_str = temp[2]
+
+#        print(irc_dict)
+
+        if irc_dict['command'] == '001':
+            await self.on_welcome(irc_dict)
+        if irc_dict['command'].lower() == 'ping':
+            await self.on_ping(irc_dict)
+        if irc_dict['command'].lower() == 'privmsg':
+            await self.on_privmsg(irc_dict)
+
+    # SEND FUNCTIONS
+    def send_raw(self, string):
+        self.writer.write('{}\r\n'.format(string).encode('utf-8'))
+#        print(string)
+
+    def send_pong(self, pong):
+        self.send_raw('PONG {}'.format(pong))
+
+    def send_nick(self, nick):
+        self.send_raw('NICK {}'.format(nick))
+
+    def send_pass(self, password):
+        self.send_raw('PASS {}'.format(password))
+
+    def send_privmsg(self, channel, string):
+        self.send_raw('PRIVMSG {} :{}'.format(channel, string))
+
+    def send_join(self, channel):
+        self.send_raw('JOIN {}'.format(channel))
+
+    # EVENT FUNCTIONS
+    async def on_welcome(self, irc_dict):
+        pass
+
+    async def on_ping(self, irc_dict):
+        try:
+            pong = irc_dict['arguments'][0]
+        except IndexError:
+            pong = ''
+        self.send_pong(pong)
+
+    async def on_privmsg(self, irc_dict):
+        pass
